92b8466310de36952df31189fc66958231fbc1c9
Felix <1842068905@qq.com>
2019-02-28 11:07:12 +0800

    analyze marco

--- analyze/analyze_marco.py
+++ analyze/analyze_marco.py
@@ -3,4 +3,29 @@ import json
 path = '../../data/marco/train_v2.1.json'
 with open(path, 'r') as file:
     data = json.load(file)
-print(len(data['query_id']))
+total = len(data['answers'])
+zero = 0
+one = 0
+two = 0
+more = 0
+para_total = 0
+for i in range(total):
+    i = str(i)
+    temp = 0
+    para_total += len(data['passages'][i])
+    for j in range(len(data['passages'][i])):
+        temp += int(data['passages'][i][j]['is_selected'])
+    if temp == 0:
+        zero += 1
+    elif temp == 1:
+        one += 1
+    elif temp == 2:
+        two += 1
+    else:
+        more += 1
+
+print(para_total, float(para_total) / total)
+print(zero, float(zero) / total)
+print(one, float(one) / total)
+print(two, float(two) / total)
+print(more, float(more) / total)
