213758a4b44f2ab4e83018d8d8cd0a0a2b67e592
Felix <1842068905@qq.com>
2019-03-10 15:53:53 +0800

    finish ptr

--- ptr_generator/model.py
+++ ptr_generator/model.py
@@ -12,15 +12,15 @@ with tf.device('/gpu:1'):
     '''
     with tf.variable_scope('bert_service', reuse=tf.AUTO_REUSE):
         bert = Bert_server()
-    marco_train = Marco_dataset(path=hp.marco_train_path)
-    marco_dev = Marco_dataset(path=hp.marco_dev_path)
-    vocab = Load_glove(hp.glove_path)
-    vocab_size = len(vocab.index2vocab)
+    vocab = Load_glove(hp.word)
+    marco_dev = Marco_dataset(path=hp.marco_dev_path, vocab=vocab)
+    #marco_train = Marco_dataset(path=hp.marco_train_path)
     '''
     para_input = tf.placeholder(dtype=tf.float32, shape=[None, hp.max_seq_length, hp.bert_embedding_size])
     qas_input = tf.placeholder(dtype=tf.float32, shape=[None, hp.max_seq_length, hp.bert_embedding_size])
-    answer = tf.placeholder(dtype=tf.float32, shape=[None, hp.max_seq_length, hp.bert_embedding_size])
-    current_answer = ['' for _ in range(hp.batch_size)]
+    answer_embd = tf.placeholder(dtype=tf.float32, shape=[None, hp.max_seq_length, hp.bert_embedding_size])
+    answer_word_embd = tf.placeholder(dtype=tf.float32, shape=[None, hp.max_seq_length, hp.bert_embedding_size])
+    answer_indices = tf.placeholder(dtype=tf.int32, shape=[None, hp.max_seq_length, 3])
     keep_prob = tf.placeholder(tf.float32)
 
     with tf.variable_scope('biAttention', reuse=tf.AUTO_REUSE):
@@ -32,13 +32,63 @@ with tf.device('/gpu:1'):
             hidden_units=hp.bert_embedding_size
         ).fuse_vector
 
-    converge_vector = tf.Variable(tf.zeros(shape=[hp.batch_size, hp.max_seq_length]))
-    word = ['' for _ in range(hp.batch_size)]
-    loss = tf.Variable(trainable=False, initial_value=tf.constant(0., dtype=tf.float32), dtype=tf.float32)
-    print(loss)
+    coverage_vector_t = tf.Variable(tf.zeros(shape=[hp.batch_size, hp.max_seq_length, 1]))
     index = 0
-    while index < hp.max_seq_length:
-        index += 1
-        ptrg = PTR_Gnerator()
-        ptr = ptrg.ptr
-        converge_vector += ptrg.attention
+
+    # all variables for attention
+    Wh = tf.Variable(tf.random_normal(shape=[4 * hp.bert_embedding_size, hp.attention_inter_size]), dtype=tf.float32)
+    Ws = tf.Variable(tf.random_normal(shape=[hp.bert_embedding_size, hp.attention_inter_size]), dtype=tf.float32)
+    wc = tf.Variable(tf.random_normal(shape=[1, hp.attention_inter_size]), dtype=tf.float32)
+    batten = tf.Variable(tf.constant(0.1, shape=[1, hp.attention_inter_size]), dtype=tf.float32)
+    V = tf.Variable(tf.random_normal(shape=[hp.attention_inter_size, 1]), dtype=tf.float32)
+
+    # all variables for pvocab
+    pvocab_w = tf.Variable(tf.random_normal(shape=[5 * hp.bert_embedding_size, hp.vocab_size]), dtype=tf.float32)
+    pvocab_b = tf.Variable(tf.constant(0.1, shape=[1, hp.vocab_size]), dtype=tf.float32)
+
+    # all variables for pointer
+    wh = tf.Variable(tf.random_normal(shape=[4 * hp.bert_embedding_size, 1]), dtype=tf.float32)
+    ws = tf.Variable(tf.random_normal(shape=[hp.bert_embedding_size, 1]), dtype=tf.float32)
+    wx = tf.Variable(tf.random_normal(shape=[hp.bert_embedding_size, 1]), dtype=tf.float32)
+    bptr = tf.Variable(tf.constant(0.1, shape=[1, 1]))
+
+    with tf.variable_scope('attention', reuse=tf.AUTO_REUSE):
+        while index < hp.max_seq_length:
+            index += 1
+            indices = []
+            for j in range(hp.batch_size):
+                indices.append([j, index])
+            st = tf.gather_nd(answer_embd, indices)
+            set = tf.tile(tf.expand_dims(st, 1), [1, hp.max_seq_length, 1])
+            xt = tf.gather_nd(answer_word_embd, indices)
+            word_indece = tf.gather_nd(answer_indices, indices)
+            ptrg = PTR_Gnerator()
+            at = ptrg.attention(
+                Wh=Wh,
+                H=fuse_vector,
+                Ws=Ws,
+                st=set,
+                wc=wc,
+                coverage=coverage_vector_t,
+                batten=batten,
+                v=V
+            )
+            coverage_vector_t = tf.add(coverage_vector_t, at)
+            attention_t = tf.tile(at, [1, 1, 4 * hp.bert_embedding_size])
+            hstar_t = tf.reduce_sum(tf.math.multiply(fuse_vector, attention_t), axis=1)
+            pvocab = ptrg.pvocab(
+                st=st,
+                hstar_t=hstar_t,
+                w=pvocab_w,
+                b=pvocab_b
+            )
+            pgen = ptrg.pointer(
+                wh=wh,
+                hstar_t=hstar_t,
+                ws=ws,
+                st=st,
+                wx=wx,
+                xt=xt,
+                bptr=bptr
+            )
+
--- ptr_generator/ptr_generator.py
+++ ptr_generator/ptr_generator.py
@@ -3,7 +3,74 @@ from hyperparameters import Hyperparameters as hp
 
 
 class PTR_Gnerator():
-    def __init__(self, qadr, ar, converge, ab):
-        self.ptr = []
-        self.attention = []
+    def attention(self, Wh, H, Ws, st, wc, coverage, batten, v):
+        H = tf.reshape(H, shape=[-1, 4 * hp.bert_embedding_size])
+        st = tf.reshape(st, shape=[-1, hp.bert_embedding_size])
+        coverage = tf.reshape(coverage, shape=[-1, 1])
+        et = tf.matmul(
+            tf.nn.tanh(
+                tf.add(
+                    tf.add(
+                        tf.add(
+                            tf.matmul(
+                                H,
+                                Wh
+                            ),
+                            tf.matmul(
+                                st,
+                                Ws
+                            )
+                        ),
+                        tf.matmul(
+                            coverage,
+                            wc
+                        )
+                    ),
+                    batten
+                )
+            ),
+            v
+        )
+        et = tf.reshape(et, [-1, hp.max_seq_length, 1])
+        at = tf.nn.softmax(et, axis=1)
+        return at
+
+    def pointer(self, wh, hstar_t, ws, st, wx, xt, bptr):
+        pgen = tf.nn.sigmoid(
+            tf.add(
+                tf.add(
+                    tf.add(
+                        tf.matmul(
+                            hstar_t,
+                            wh
+                        ),
+                        tf.matmul(
+                            st,
+                            ws
+                        )
+                    ),
+                    tf.matmul(
+                        xt,
+                        wx
+                    )
+                ),
+                bptr
+            )
+        )
+        return pgen
+
+    def pvocab(self, st, hstar_t, w, b):
+        pvocab_pre = tf.concat([hstar_t, st], axis=1)
+        pvocab = tf.nn.softmax(
+            tf.nn.tanh(
+                tf.add(
+                    tf.matmul(
+                        pvocab_pre,
+                        w
+                    ),
+                    b
+                )
+            )
+        )
+        return pvocab
 
