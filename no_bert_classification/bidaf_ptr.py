fa458d97117f536d2c1fb00b9adb19067df8a33a
Felix <1842068905@qq.com>
2019-03-22 17:21:12 +0800

    no while loop

--- no_bert_classification/bidaf_ptr.py
+++ no_bert_classification/bidaf_ptr.py
@@ -24,69 +24,6 @@ def convert2word_embd(words):
         temp.append([0. for _ in range(hp.embedding_size)])
     return np.array(temp)
 
-
-def cond(LOSS, index):
-    return index < hp.max_seq_length
-
-
-def get_loss(LOSS, index):
-    global ans_lstm, answer_word_embedding, answer_indices, Wh, fuse_vector, Ws, wc, coverage_vector_t
-    global batten, V, pvocab_w, pvocab_b, wh, ws, wx, bptr, vocab_size
-    ptrg = PTR_Gnerator(
-        bert_embedding_size=hp.embedding_size,
-        max_seq_length=hp.max_seq_length,
-        ptr_conv_beta=hp.ptr_conv_beta
-    )
-    indices = [index]
-    st = tf.gather_nd(ans_lstm, indices)
-    set = tf.expand_dims(st, axis=0)
-    xt = tf.gather_nd(answer_word_embedding, indices)
-    word_indice = tf.gather_nd(answer_indices, indices)
-    at = ptrg.attention(
-        Wh=Wh,
-        H=fuse_vector,
-        Ws=Ws,
-        st=set,
-        wc=wc,
-        coverage=coverage_vector_t,
-        batten=batten,
-        v=V
-    )
-
-    coverage_vector_t = tf.add(coverage_vector_t, at)
-    attention_t = tf.tile(at, [1, 8 * hp.embedding_size])
-    h_star_t = tf.reduce_sum(tf.multiply(fuse_vector, attention_t), axis=0)
-
-
-    pvocab = ptrg.pvocab(
-        hstar_t=h_star_t,
-        st=st,
-        w=pvocab_w,
-        b=pvocab_b
-    )
-    p_overall = tf.concat([pvocab, tf.reshape(at, shape=[-1, hp.max_seq_length])], axis=1)
-
-    pgen = ptrg.pointer(
-        wh=wh,
-        hstar_t=tf.expand_dims(h_star_t, axis=0),
-        ws=ws,
-        st=tf.expand_dims(st, axis=0),
-        wx=wx,
-        xt=tf.expand_dims(xt, axis=0),
-        bptr=bptr
-    )
-
-    LOSS += ptrg.loss(
-        p_overall=p_overall,
-        words_indice=tf.expand_dims(word_indice, axis=0),
-        vocab_size=vocab_size,
-        pgen=pgen,
-        at=at,
-        coverage_vector_t=coverage_vector_t
-    )
-
-    return LOSS, index + 1
-
 '''
 vocab = load_dict(path=hp.word, embedding_size=hp.embedding_size)
 marco_dev = load_marco(
@@ -105,7 +42,7 @@ marco_train = load_marco(
 )
 '''
 
-with tf.device('/cpu:1'):
+with tf.device('/cpu'):
     with tf.variable_scope('embedding'):
         embedding_weight = tf.Variable(tf.constant(0.0, shape=[hp.vocab_size, hp.embedding_size]), trainable=False)
         embedding_placeholder = tf.placeholder(tf.float32, [hp.vocab_size, hp.embedding_size])
@@ -158,33 +95,87 @@ with tf.device('/cpu:1'):
         ).fuse_vector
         fuse_vector = tf.reshape(fuse_vector, shape=[hp.max_seq_length, 8 * hp.embedding_size])
 
-    coverage_vector_t = tf.Variable(tf.constant(0., shape=[hp.max_seq_length, 1]), dtype=tf.float32, trainable=False)
+    with tf.variable_scope('ptr_variables', reuse=tf.AUTO_REUSE):
+        coverage_vector_t = tf.Variable(tf.constant(0., shape=[hp.max_seq_length, 1]), dtype=tf.float32, trainable=False)
 
-    # all variables for attention
-    Wh = tf.Variable(tf.random_normal(shape=[8 * hp.embedding_size, hp.attention_inter_size]), dtype=tf.float32)
-    Ws = tf.Variable(tf.random_normal(shape=[2 * hp.embedding_size, hp.attention_inter_size]), dtype=tf.float32)
-    wc = tf.Variable(tf.random_normal(shape=[1, hp.attention_inter_size]), dtype=tf.float32)
-    batten = tf.Variable(tf.constant(0.1, shape=[1, hp.attention_inter_size]), dtype=tf.float32)
-    V = tf.Variable(tf.random_normal(shape=[hp.attention_inter_size, 1]), dtype=tf.float32)
+        # all variables for attention
+        Wh = tf.Variable(tf.random_normal(shape=[8 * hp.embedding_size, hp.attention_inter_size]), dtype=tf.float32)
+        Ws = tf.Variable(tf.random_normal(shape=[2 * hp.embedding_size, hp.attention_inter_size]), dtype=tf.float32)
+        wc = tf.Variable(tf.random_normal(shape=[1, hp.attention_inter_size]), dtype=tf.float32)
+        batten = tf.Variable(tf.constant(0.1, shape=[1, hp.attention_inter_size]), dtype=tf.float32)
+        V = tf.Variable(tf.random_normal(shape=[hp.attention_inter_size, 1]), dtype=tf.float32)
 
-    # all variables for pvocab
-    pvocab_w = tf.Variable(tf.random_normal(shape=[10 * hp.embedding_size, hp.vocab_size]), dtype=tf.float32)
-    pvocab_b = tf.Variable(tf.constant(0.1, shape=[1, hp.vocab_size]), dtype=tf.float32)
+        # all variables for pvocab
+        pvocab_w = tf.Variable(tf.random_normal(shape=[10 * hp.embedding_size, hp.vocab_size]), dtype=tf.float32)
+        pvocab_b = tf.Variable(tf.constant(0.1, shape=[1, hp.vocab_size]), dtype=tf.float32)
 
-    # all variables for pointer
-    wh = tf.Variable(tf.random_normal(shape=[8 * hp.embedding_size, 1]), dtype=tf.float32)
-    ws = tf.Variable(tf.random_normal(shape=[2 * hp.embedding_size, 1]), dtype=tf.float32)
-    wx = tf.Variable(tf.random_normal(shape=[hp.embedding_size, 1]), dtype=tf.float32)
-    bptr = tf.Variable(tf.constant(0.1, shape=[1, 1]), dtype=tf.float32)
+        # all variables for pointer
+        wh = tf.Variable(tf.random_normal(shape=[8 * hp.embedding_size, 1]), dtype=tf.float32)
+        ws = tf.Variable(tf.random_normal(shape=[2 * hp.embedding_size, 1]), dtype=tf.float32)
+        wx = tf.Variable(tf.random_normal(shape=[hp.embedding_size, 1]), dtype=tf.float32)
+        bptr = tf.Variable(tf.constant(0.1, shape=[1, 1]), dtype=tf.float32)
 
-    # variable for calculate loss
-    vocab_size = tf.Variable(tf.constant(hp.vocab_size, shape=[1]), trainable=False)
+        # variable for calculate loss
+        vocab_size = tf.Variable(tf.constant(hp.vocab_size, shape=[1]), trainable=False)
 
     LOSS = tf.constant(0., shape=[1])
+    index = 0
 
     with tf.variable_scope('ptr_generator', reuse=tf.AUTO_REUSE):
-        index = 0
-        LOSS, index = tf.while_loop(cond, get_loss, [LOSS, index], parallel_iterations=1)
+        while index < hp.max_seq_length:
+            ptrg = PTR_Gnerator(
+                bert_embedding_size=hp.embedding_size,
+                max_seq_length=hp.max_seq_length,
+                ptr_conv_beta=hp.ptr_conv_beta
+            )
+            indices = [index]
+            st = tf.gather_nd(ans_lstm, indices)
+            set = tf.expand_dims(st, axis=0)
+            xt = tf.gather_nd(answer_word_embedding, indices)
+            word_indice = tf.gather_nd(answer_indices, indices)
+            at = ptrg.attention(
+                Wh=Wh,
+                H=fuse_vector,
+                Ws=Ws,
+                st=set,
+                wc=wc,
+                coverage=coverage_vector_t,
+                batten=batten,
+                v=V
+            )
+
+            coverage_vector_t = tf.add(coverage_vector_t, at)
+            attention_t = tf.tile(at, [1, 8 * hp.embedding_size])
+            h_star_t = tf.reduce_sum(tf.multiply(fuse_vector, attention_t), axis=0)
+
+            pvocab = ptrg.pvocab(
+                hstar_t=h_star_t,
+                st=st,
+                w=pvocab_w,
+                b=pvocab_b
+            )
+            p_overall = tf.concat([pvocab, tf.reshape(at, shape=[-1, hp.max_seq_length])], axis=1)
+
+            pgen = ptrg.pointer(
+                wh=wh,
+                hstar_t=tf.expand_dims(h_star_t, axis=0),
+                ws=ws,
+                st=tf.expand_dims(st, axis=0),
+                wx=wx,
+                xt=tf.expand_dims(xt, axis=0),
+                bptr=bptr
+            )
+
+            LOSS += ptrg.loss(
+                p_overall=p_overall,
+                words_indice=tf.expand_dims(word_indice, axis=0),
+                vocab_size=vocab_size,
+                pgen=pgen,
+                at=at,
+                coverage_vector_t=coverage_vector_t
+            )
+
+            index += 1
 
     LOSS = tf.reduce_sum(LOSS, axis=0)
     loss = LOSS / hp.max_seq_length
@@ -192,8 +183,11 @@ with tf.device('/cpu:1'):
 
     init = tf.global_variables_initializer()
 
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+
     '''
-    with tf.Session() as sess:
+    with tf.Session(config=config) as sess:
         sess.run(init)
         for epoch in range(hp.epoch):
             for i in range(marco_dev.total):
